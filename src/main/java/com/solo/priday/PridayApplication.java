af75ba2d3cc5a838b70a3215c4d1019052f419eb
최한윤 <chlgksdbs98@naver.com>
2024-12-29 18:02:13 +0900

    init: create project

---
+++ src/main/java/com/solo/priday/PridayApplication.java
@@ -0,0 +1,13 @@
+package com.solo.priday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PridayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PridayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solo/priday/PridayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solo.priday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PridayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
